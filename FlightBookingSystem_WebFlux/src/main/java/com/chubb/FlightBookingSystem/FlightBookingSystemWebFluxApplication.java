e8bb5c0f69fc2cbfe542e152ec39958d584ab16e
isananya <nayakananya2004@gmail.com>
2025-11-22 18:50:42 +0530

    first commit

---
+++ FlightBookingSystem_WebFlux/src/main/java/com/chubb/FlightBookingSystem/FlightBookingSystemWebFluxApplication.java
@@ -0,0 +1,13 @@
+package com.chubb.FlightBookingSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightBookingSystemWebFluxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightBookingSystemWebFluxApplication.class, args);
+	}
+
+}
---
+++ FlightBookingSystem_WebFlux/src/test/java/com/chubb/FlightBookingSystem/FlightBookingSystemWebFluxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chubb.FlightBookingSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightBookingSystemWebFluxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
